9644e2d89890881f8cfd76cd4d5e5ca77c240eaf
Ashok Kandpal <ashokkandpal845@gmail.com>
2023-11-28 10:51:36 +0530

    Adding comments to the code

--- SlidingWindow/MaxSumSubArray.cpp
+++ SlidingWindow/MaxSumSubArray.cpp
@@ -1,14 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+//Function to find the maximum sum of window of size k
 int maxSumSubArray(vector<int> arr , int k){
     int maxSum = 0, sum = 0;
     int n = arr.size();
-    for(int i = 0 ; i < k ; i++) sum+=arr[i];
+    for(int i = 0 ; i < k ; i++) sum+=arr[i];    // Loop to find the sum of the first window of size k
     maxSum = max(maxSum, sum);
-    for(int i = k ; i < n ; i++){
-        sum+=arr[i];
-        sum-=arr[i-k];
-        maxSum = max(maxSum, sum);
+    for(int i = k ; i < n ; i++){           // loop tp go from first window to last window
+        sum+=arr[i];                        // add the last element of the  current window
+        sum-=arr[i-k];                      // Remove the last element of the previous window
+        maxSum = max(maxSum, sum);          // Compare the sum of the current window with previous window to obtain the maximum sum
     }
     return maxSum;
 }
